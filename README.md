d8ab9b6168e7403ac3481bdfb536f01ec108939b
GittinsGood <cgs.orangutan@gmail.com>
2023-12-08 13:59:45 -0500

    Created main and interpreter classes.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bfvm
 An obfuscated language interpreter/virtual machine
+
+based off https://en.wikipedia.org/wiki/Brainfuck 
+
+Created as a learning exercise, the aim of this project was to write a small interpreter for the language.
+
+There are probably many much better versions by other people on git.
---
+++ src/Bfvm.java
@@ -0,0 +1,57 @@
+public class Bfvm {
+
+    private final int cellSize = 30000; // Specified size is 30,000 bytes.
+    private final byte[] cells = new byte[cellSize];
+    private int datapointer = 0;
+    private int progpointer = 0;
+    private final char[] program;
+
+    public Bfvm(char[] program) {
+        this.program = program;
+        System.out.println(">" + (run() == 0 ? "OK." : "Error"));
+    }
+
+    private int run() {
+        
+        try {
+            while (progpointer < program.length) {
+
+                switch (program[progpointer]) {
+                    case '>' -> { datapointer++; if (datapointer == cellSize) datapointer = 0;}
+                    case '<' -> { datapointer--; if (datapointer == -1) datapointer = cellSize-1;}
+                    case '+' -> cells[datapointer]++;
+                    case '-' -> cells[datapointer]--;
+                    case '.' -> System.out.print((char)cells[datapointer]);
+                    case ',' -> cells[datapointer] = (byte) System.in.read();
+                    case '[' -> { if (cells[datapointer] == 0) progJump(1); }
+                    case ']' -> { if (cells[datapointer] != 0) progJump(-1); }
+                }
+                progpointer++;
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            return 1;
+        }
+        return 0;
+    }
+
+    private void progJump(int direction) throws Exception {
+        int loopdepth = 1;
+        while (loopdepth > 0) {
+            progpointer += direction;
+            if (progpointer == -1) progpointer = program.length-1;
+            if (progpointer ==  program.length) progpointer = 0;
+            if (progpointer < 0 || progpointer > program.length) {
+                throw(new Exception("Program pointer out of bounds while traversing loop."));
+            }
+            char c = program[progpointer];
+            // Whichever way the loop is going the loop counter incs/decs when the loop chats are found.
+            loopdepth +=
+            switch (c) {
+                case '[' -> direction;
+                case ']' -> direction*-1;
+                default -> 0;
+            };
+        }
+    }
+}
